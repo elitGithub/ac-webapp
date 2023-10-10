5370fd9e2198e12257e2835badd715a5a178d3bb
fafnir-kun <senpaikouhai02@gmail.com>
2023-10-10 12:41:42 +0800

    Audio Manager

---
+++ src/Engine/audio/audiomanager.ts
@@ -0,0 +1,153 @@
+import { Howl } from "howler";
+
+interface SoundEffect {
+    [key: string]: Howl;
+}
+
+interface BackgroundMusicOptions {
+    loop: boolean;
+    id: string;
+    loopStart?: number; // Loop start time in seconds
+    volume?: number;
+    fade?: number; // Fade-in duration in milliseconds
+}
+
+export class AudioManager {
+    private backgroundMusic: Howl | null = null;
+    private soundEffects: SoundEffect = {};
+
+    // Load and play background music
+    playBackgroundMusic(src: string, options?: BackgroundMusicOptions): void {
+        if (this.backgroundMusic) {
+            this.backgroundMusic.stop();
+            this.backgroundMusic.unload();
+        }
+
+        const defaultOptions: BackgroundMusicOptions = {
+            loop: false,
+            id: "",
+            loopStart: 0,
+            volume: 0.8,
+            fade: 0, // Default to no fade-in
+        };
+
+        options = { ...defaultOptions, ...options };
+
+        this.backgroundMusic = new Howl({
+            src: [src],
+            volume: 0.01, // Start with volume 0 for fade-in
+            preload: true,
+            onload: () => {
+                if (this.backgroundMusic) {
+                    // Ensure the audio is fully loaded before accessing duration
+                    const duration = this.backgroundMusic.duration();
+
+                    this.backgroundMusic._sprite = {
+                        loop: [options?.loopStart || 0, duration * 1000, true],
+                    };
+
+                    let id = this.backgroundMusic.play("loop");
+
+                    if (options?.fade && options.fade > 0) {
+                        // Apply fade-in effect if specified
+                        this.backgroundMusic.fade(
+                            0,
+                            options.volume!,
+                            options.fade,
+                            id
+                        );
+                    } else {
+                        // Start playing without fade-in
+                        this.backgroundMusic.volume(options?.volume!);
+                    }
+                }
+            },
+            onplay: () => {},
+            onloaderror: (id, error) => {
+                console.error(
+                    `Error loading background music: id=${id} error=${error}`
+                );
+            },
+            onplayerror: (id, error) => {
+                console.error(
+                    `Error playing background music: id=${id} error=${error}`
+                );
+            },
+        });
+    }
+
+    // Stop the background music
+    stopBackgroundMusic() {
+        if (this.backgroundMusic) {
+            this.backgroundMusic.stop();
+        }
+    }
+
+    // Pause the background music
+    pauseBackgroundMusic(): void {
+        if (this.backgroundMusic) {
+            this.backgroundMusic.pause();
+        }
+    }
+
+    // Resume the background music
+    resumeBackgroundMusic(): void {
+        if (this.backgroundMusic) {
+            this.backgroundMusic.play();
+        }
+    }
+
+    playSoundEffect(src: string, volume: number = 1.0, fade?: number): void {
+        const name = this.getSoundEffectNameFromSrc(src);
+
+        if (this.soundEffects[name]) {
+            this.soundEffects[name].unload();
+        }
+
+        this.soundEffects[name] = new Howl({
+            src: [src],
+            volume: 0, // Start with volume 0 for fade-in
+            onend: () => {
+                // Handle sound effect end
+            },
+        });
+
+        if (fade && fade > 0) {
+            // Apply fade-in effect if specified
+            this.soundEffects[name].fade(0, volume, fade);
+        } else {
+            // Start playing without fade-in
+            this.soundEffects[name].volume(volume);
+        }
+
+        this.soundEffects[name].play();
+    }
+
+    // Stop a specific sound effect. name is defined as the audio file name.
+    stopSoundEffect(name: string): void {
+        if (this.soundEffects[name]) {
+            this.soundEffects[name].stop();
+        }
+    }
+
+    // Stop all sound effects
+    stopAllSoundEffects(): void {
+        for (const name in this.soundEffects) {
+            if (Object.prototype.hasOwnProperty.call(this.soundEffects, name)) {
+                this.soundEffects[name].stop();
+                this.soundEffects[name].unload();
+                delete this.soundEffects[name];
+            }
+        }
+    }
+
+    private getSoundEffectNameFromSrc(src: string): string {
+        // Extract the file name from the src (assuming file names are unique)
+        const fileName = src.split("/").pop() || "";
+
+        // Remove file extension (assuming the extension is .ogg)
+        const name = fileName.replace(".ogg", "");
+
+        return name;
+    }
+}
