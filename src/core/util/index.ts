ab428445c625c341874dec37f45f8d2d3aa8d0ff
szokoz <epiclegend12@live.co.uk>
2023-10-30 22:04:57 +0000

    Generate accurate hitmaps
    Excludes transparent pixels of a sprite being used to detect collision with the mouse. E.g. Home kitchen stairs

--- src/core/util/index.ts
+++ src/core/util/index.ts
@@ -1,1 +1,1 @@
-export const Unimplemented = null;
+export * from "./genSpriteHitmap";
--- src/engine/coreentities/baseinteractable.ts
+++ src/engine/coreentities/baseinteractable.ts
@@ -1,5 +1,6 @@
-import { BLEND_MODES, Container, Sprite, Texture } from "pixi.js";
+import { BLEND_MODES, Container, IPointData, Sprite, Texture } from "pixi.js";
 import { BaseEntity } from "./entity";
+import { genHitmap } from "../../core/util";
 
 export interface BaseInteractableAction {
     action: string,
@@ -79,5 +80,59 @@ export class BaseInteractable extends Sprite implements BaseEntity {
 
     onPointerHoverEnd(event: any) {
         this.blendMode = BLEND_MODES.NORMAL;
+    }
+
+    containsPoint(point: IPointData): boolean {
+        const tempPoint = { x: 0, y: 0 }
+        //get mouse poisition relative to the anchor point
+        this.worldTransform.applyInverse(point, tempPoint);
+        // console.log('temppoint:' + tempPoint);
+
+        const width = this._texture.orig.width;
+        const height = this._texture.orig.height;
+        const x1 = -width * this.anchor.x;
+        let y1 = 0;
+
+        let flag = false;
+        //bounding box collision detection for sprite
+        if (tempPoint.x >= x1 && tempPoint.x < x1 + width) {
+            y1 = -height * this.anchor.y;
+
+            if (tempPoint.y >= y1 && tempPoint.y < y1 + height) {
+                flag = true;
+            }
+        }
+        //if collision not detected return false
+        if (!flag) {
+            return false
+        }
+
+        // bitmap check
+        const tex = this.texture;
+        const baseTex = this.texture.baseTexture;
+        const res = baseTex.resolution;
+
+        if (!baseTex.hitmap) {
+            //generate hitmap
+            if (!genHitmap(baseTex, 255)) {
+                return true;
+            }
+
+        }
+
+        const hitmap = baseTex.hitmap;
+
+        // console.log(hitmap)
+        // this does not account for rotation yet
+
+        //check mouse position if its over the sprite and visible
+        let dx = Math.round((tempPoint.x - x1 + tex.frame.x) * res);
+        let dy = Math.round((tempPoint.y - y1 + tex.frame.y) * res);
+        // console.log(dx);
+        // console.log(dy);
+        let ind = (dx + dy * baseTex.realWidth);
+        let ind1 = ind % 32;
+        let ind2 = ind / 32 | 0;
+        return (hitmap[ind2] & (1 << ind1)) !== 0;
     }
 }
