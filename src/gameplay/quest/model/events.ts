0b1cc497569b04cb069c60b9ad051ae23ec5fba6
szokoz <epiclegend12@live.co.uk>
2023-11-18 02:46:25 +0000

    quest additions

--- src/gameplay/quest/model/events.ts
+++ src/gameplay/quest/model/events.ts
@@ -2,9 +2,12 @@ import { IEngineEvent } from "../../../engine";
 
 export const START_QUEST = Symbol("QUEST_REQ_START_EVENT");
 export const ADVANCE_QUEST = Symbol("QUEST_REQ_ADVANCE_EVENT");
+export const COMPLETE_QUEST = Symbol("QUEST_REQ_COMPLETE_EVENT");
 
 export const QUEST_STARTED = Symbol("QUEST_STARTED_EVENT");
 export const QUEST_STEP_STARTED = Symbol("QUEST_STEP_STARTED");
+export const QUEST_COMPLETED = Symbol("QUEST_COMPLETED_EVENT");
+export const QUEST_FAILED = Symbol("QUEST_FAILED_EVENT");
 
 
 export interface StartQuestEvent extends IEngineEvent {
