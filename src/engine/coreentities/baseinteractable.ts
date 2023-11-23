6c5942b0c728dd4ceef4f5e1cb0ca97a67ab9185
szokoz <epiclegend12@live.co.uk>
2023-11-23 17:14:01 +0000

    baseents hierarchy

--- src/engine/coreentities/baseinteractable.ts
+++ src/engine/coreentities/baseinteractable.ts
@@ -1,5 +1,5 @@
 import { BLEND_MODES, Container, IPointData, Sprite, Texture } from "pixi.js";
-import { BaseEntity } from "./entity";
+import { BaseEntity, RenderableEntity } from "./entity";
 import { genHitmap, onSceneOutChildren } from "../../core/util";
 import { Popup } from "../gui";
 import { Scene } from "../scene/models";
@@ -9,8 +9,7 @@ export interface BaseInteractableAction {
     handler: Function,
 }
 
-export class BaseInteractable extends Sprite implements BaseEntity {
-    public name: string;
+export class BaseInteractable extends RenderableEntity {
     public actions: [BaseInteractableAction];
     public canInteract: boolean = true;
     public hoverHighlight = true;
@@ -18,23 +17,38 @@ export class BaseInteractable extends Sprite implements BaseEntity {
     private label?: Popup;
 
     constructor(baseTexture?: Texture, name?: string, action?: BaseInteractableAction) {
-        super(baseTexture);
-        this.name = name ?? "";
+        super(baseTexture, name);
         this.actions = [{action: "interact", handler: () => {console.log("hi")}}];
         if (action) {
             this.actions.push(action);
         }
 
-        this.on("pointerdown", this.onPointerPress.bind(this));
-        this.on("pointerup", this.onPointerRelease.bind(this));
-        this.on("pointerupoutside", this.onPointerCancel.bind(this));
-        this.on("pointercancel", this.onPointerCancel.bind(this));
-        this.on("pointertap", this.onPointerClick.bind(this));
-
-        this.on("pointerover", this.onPointerHover.bind(this));
-        this.on("pointerout", this.onPointerHoverEnd.bind(this));
-
         this.eventMode = "dynamic";
+    }
+
+    addAction(action: BaseInteractableAction) {
+        this.actions.push(action);
+    }
+
+    invokeAction(action: string) {
+        const deleteActions = [];
+        for (let i = 0; i < this.actions.length; i++) {
+            const biAction = this.actions[i];
+            if (biAction.action !== action) {
+                continue;
+            }
+
+            if (biAction.handler) {
+                biAction.handler(this);
+            }
+            else {
+                deleteActions.push(i);
+            }
+        }
+
+        for (let i = 0; i < deleteActions.length; i++) {
+            this.actions.splice(deleteActions[i], 1);
+        }
     }
 
     onSceneOut(scene: Scene): void {
@@ -52,24 +66,7 @@ export class BaseInteractable extends Sprite implements BaseEntity {
             return;
         }
 
-        const deleteActions = [];
-        for (let i = 0; i < this.actions.length; i++) {
-            const biAction = this.actions[i];
-            if (biAction.action !== "interact") {
-                continue;
-            }
-
-            if (biAction.handler) {
-                biAction.handler(this);
-            }
-            else {
-                deleteActions.push(i);
-            }
-        }
-
-        for (let i = 0; i < deleteActions.length; i++) {
-            this.actions.splice(deleteActions[i], 1);
-        }
+        this.invokeAction("interact");
     }
 
     onPointerClick(event: any) {
--- src/engine/coreentities/entity.ts
+++ src/engine/coreentities/entity.ts
@@ -1,7 +1,58 @@
+import { Sprite, Texture } from "pixi.js";
 import { Scene } from "../scene/models";
 
 export interface BaseEntity {
     name: string;
 
     onSceneOut(scene: Scene): void;
+}
+
+export class RenderableEntity extends Sprite implements BaseEntity {
+    name: string;
+
+    constructor(baseTexture?: Texture, name?: string) {
+        super(baseTexture);
+        this.name = name ?? "";
+
+        this.on("pointerdown", this.onPointerPress.bind(this));
+        this.on("pointerup", this.onPointerRelease.bind(this));
+        this.on("pointerupoutside", this.onPointerCancel.bind(this));
+        this.on("pointercancel", this.onPointerCancel.bind(this));
+        this.on("pointertap", this.onPointerClick.bind(this));
+
+        this.on("pointerover", this.onPointerHover.bind(this));
+        this.on("pointerout", this.onPointerHoverEnd.bind(this));
+    }
+
+    onPointerPress(event: any) {
+
+    }
+
+    onPointerRelease(event: any) {
+
+    }
+
+    onPointerClick(event: any) {
+
+    }
+
+    onPointerCancel(event: any) {
+
+    }
+
+    onPointerHover(event: any) {
+
+    }
+
+    onPointerHoverEnd(event: any) {
+
+    }
+
+    onSceneOut(scene: Scene): void {
+    }
+
+}
+
+export class LogicEntity {
+
 }
