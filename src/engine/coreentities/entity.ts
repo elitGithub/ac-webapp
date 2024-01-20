a2ecb38e11bffef9e9e436785124424e4911da62
szokoz <epiclegend12@live.co.uk>
2024-01-20 01:07:47 +0000

    entity system

--- src/engine/coreentities/entity.ts
+++ src/engine/coreentities/entity.ts
@@ -1,19 +1,30 @@
-import { Sprite, Texture } from "pixi.js";
+import { IDestroyOptions, Sprite, Texture } from "pixi.js";
 import { Scene } from "../scene/models";
 import { getEngine } from "..";
 
 export interface BaseEntity {
     name: string;
+    markedForDeletion: boolean;
+    deletionOptions: IDestroyOptions;
 
     onSceneOut(scene: Scene): void;
+    destroyEntity(destroyTextures?: boolean): void;
 }
 
 export class RenderableEntity extends Sprite implements BaseEntity {
     name: string;
+    markedForDeletion: boolean;
+    deletionOptions: IDestroyOptions;
 
     constructor(baseTexture?: Texture, name?: string) {
         super(baseTexture);
         this.name = name ?? "";
+        this.markedForDeletion = false;
+        this.deletionOptions = {
+            children: true,
+            texture: false,
+            baseTexture: false
+        }
 
         this.on("pointerdown", this.onPointerPress.bind(this));
         this.on("pointerup", this.onPointerRelease.bind(this));
@@ -24,7 +35,7 @@ export class RenderableEntity extends Sprite implements BaseEntity {
         this.on("pointerover", this.onPointerHover.bind(this));
         this.on("pointerout", this.onPointerHoverEnd.bind(this));
 
-        getEngine().getGame().gameEntities.push(this);
+        getEngine().getEnt().addEntityToList(this);
     }
 
     onPointerPress(event: any) {
@@ -52,6 +63,14 @@ export class RenderableEntity extends Sprite implements BaseEntity {
     }
 
     onSceneOut(scene: Scene): void {
+    }
+
+    destroyEntity(destroyTextures?: boolean): void {
+        if (destroyTextures) {
+            this.deletionOptions.texture = destroyTextures;
+            this.deletionOptions.baseTexture = destroyTextures;
+        }
+        this.markedForDeletion = true;
     }
 
 }
---
+++ src/engine/engineents.ts
@@ -0,0 +1,79 @@
+import { Container } from "pixi.js";
+import { EngineSystem, IEngineEvent } from ".";
+import { BaseEntity } from "./coreentities";
+
+export class EntitySystem implements EngineSystem {
+    ents: Map<BaseEntity, string>;
+
+    constructor() {
+        this.ents = new Map<BaseEntity, string>();
+    }
+
+    addEntityToList(entity: BaseEntity) {
+        if (this.ents.has(entity)) {
+            return;
+        }
+
+        for(const name of this.ents.values()) {
+            if (name === entity.name) {
+                console.error("Tried to add entity but its name matches an existing entity.")
+                return;
+            }
+        }
+
+        this.ents.set(entity, entity.name);
+    }
+
+    findEntityByName(name: string) {
+        for(const [ent, entName] of this.ents.entries()) {
+            if (entName === name) {
+                return ent;
+            }
+        }
+    }
+
+    findEntitiesByType<T>(type: any): T[] {
+        const ents: any[] = [];
+        for (const ent of this.ents.keys()) {
+            if (ent instanceof type) {
+                ents.push(ent);
+            }
+        }
+
+        return ents;
+    }
+
+    removeEntityFromList(entity?: BaseEntity, name?: string) {
+        if (entity) {
+            this.ents.delete(entity);
+        }
+        else if (name) {
+            const ent = this.findEntityByName(name);
+            if (ent) {
+                this.ents.delete(ent);
+            }
+        }
+    }
+
+    queue(engineEvent: IEngineEvent): void {
+    }
+
+    update(time: number): void {
+        for (const ent of this.ents.keys()) {
+            if (ent.markedForDeletion) {
+                if (ent instanceof Container) {
+                    ent.destroy(ent.deletionOptions);
+                }
+
+                this.removeEntityFromList(ent);
+            }
+        }
+    }
+
+    loadState?(data: EntitySystem): void {
+        if (data.ents) {
+            this.ents = data.ents;
+        }
+    }
+
+}
